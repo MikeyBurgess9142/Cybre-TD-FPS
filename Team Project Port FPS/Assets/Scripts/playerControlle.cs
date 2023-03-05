3e78951371f317f0231d4b27b2dbeebe3d69b6f4
MikeyBurgess9142 <MJBurgess@student.fullsail.edu>
2023-03-05 14:57:19 -0500

    player moves and jumps
    player moves and jumps and is written inside a movement function

--- Team Project Port FPS/Assets/Scripts/playerControlle.cs
+++ Team Project Port FPS/Assets/Scripts/playerControlle.cs
@@ -24,7 +24,22 @@ public class playerControlle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        movement();
+    }
+
+    void movement()
+    {
         move = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+        move = move.normalized;
         controller.Move(move * Time.deltaTime * playerSpd);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            playerVeloc.y = jumpSpd;
+        }
+
+        playerVeloc.y -= gravity * Time.deltaTime;
+
+        controller.Move(playerVeloc * Time.deltaTime);
     }
 }
