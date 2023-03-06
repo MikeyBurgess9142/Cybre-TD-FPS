110b1a928511ee7bd647d2e0d85bc0c0a9de7e26
MikeyBurgess9142 <MJBurgess@student.fullsail.edu>
2023-03-06 01:23:33 -0500

    wall and ground
    made a wall prefab and made grass ground walkable

--- Team Project Port FPS/Assets/Scripts/enemyAI.cs
+++ Team Project Port FPS/Assets/Scripts/enemyAI.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemyAI : MonoBehaviour, IDamage
 {
     [Header("---Components---")]
     [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
 
     [Header("---Enemy Stats---")]
     [Range(5, 500)] [SerializeField] int HP;
@@ -28,6 +30,8 @@ public class enemyAI : MonoBehaviour, IDamage
     // Update is called once per frame
     void Update()
     {
+        agent.SetDestination(gameManager.instance.player.transform.position);
+
         if (!isShooting)
         {
             StartCoroutine(shoot());
