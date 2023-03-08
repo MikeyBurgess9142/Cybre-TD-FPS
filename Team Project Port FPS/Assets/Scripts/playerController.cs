bdf9865679540acf112e0487f933e03d0bf5683a
MikeyBurgess9142 <MJBurgess@student.fullsail.edu>
2023-03-08 09:04:19 -0500

    fixed player stats
    jumped way too high fell way too slow put back to settings that felt like realistic jump height and gravity fall rate

--- Team Project Port FPS/Assets/Scripts/playerController.cs
+++ Team Project Port FPS/Assets/Scripts/playerController.cs
@@ -11,8 +11,8 @@ public class playerController : MonoBehaviour
     [Range(10, 1000)] [SerializeField] int HP;
     [Range(5, 30)] [SerializeField] float playerSpd;
     [Range(1, 10)] [SerializeField] int jumpMax;
-    [Range(5, 50)] [SerializeField] int jumpSpd;
-    [Range(5, 50)] [SerializeField] int gravity;
+    [Range(1,25)] [SerializeField] int jumpSpd;
+    [Range(10,10)] [SerializeField] int gravity;
 
     [Header("---Gun Stats---")]
     [Range(0, 10)] [SerializeField] float shtRate;
